efccfdd470dbe8c8b096dd43c53acb150f66165a
Nguyen Cao Thang <nguyencaothangtp@gmail.com>
2019-01-07 21:48:23 +0700

    Add routing table

--- Core/Router.php
+++ Core/Router.php
@@ -1,5 +1,29 @@
 <?php
 
 class Router {
+    /**
+     * Associate array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param $route
+     * @param $params
+     */
+    public function add($route, $params) {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes() {
+        return $this->routes;
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -3,8 +3,14 @@
 require '../Core/Router.php';
 
 $router = new Router();
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 
-echo get_class($router);
+
+echo '<pre>';
+print_r($router->getRoutes());
+echo '</pre>';
 
 
 ?>
