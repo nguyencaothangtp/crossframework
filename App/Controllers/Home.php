46ca612ab5cc991b09e29d0a734a0108e2f4e102
Nguyen Cao Thang <nguyencaothangtp@gmail.com>
2019-01-21 16:58:39 +0700

    Add Views

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -2,17 +2,21 @@
 
 namespace App\Controllers;
 
+use \Core\View;
+
 class Home extends \Core\Controller {
 
     public function indexAction() {
-        echo 'I am in index action of Home controller';
+//        echo 'I am in index action of Home controller';
+        $data['test'] = '<script>alert(1)</script>';
+        View::render('Home/index.php', $data);
     }
 
     protected function before() {
-        echo '(before)';
+        //echo '(before)';
     }
 
     protected function after() {
-        echo ' (after)';
+        //echo ' (after)';
     }
 }
